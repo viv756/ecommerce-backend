64fb5576742b2cee4b9cd8f2d4a7f5726e1a0fbb
*vivek <vrvivekofficial@gmail.com>
2025-01-16 14:44:20 +0530

    create auth Middleware

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { deleteUser, getAllUsers, getUser, newUser } from "../controllers/userController.js";
+import { adminOnly } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/new", newUser);
-router.get("/all", getAllUsers);
-router.get("/:id").get("/:id", getUser).delete("/:id", deleteUser);
+router.get("/all", adminOnly, getAllUsers);
+router.get("/:id").get("/:id", getUser).delete("/:id", adminOnly, deleteUser);
 
 export default router;
